6e4dc42f1d4ab2bab63f2b29d85c6b6ba2e0302a
Debnathweb <59193140+Debnathweb@users.noreply.github.com>
2024-04-02 12:24:33 +0530

    test table

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,63 @@
-import { MDXEditor } from '@mdxeditor/editor'
-import '@mdxeditor/editor/style.css'
+import React from "react";
+import {
+  MDXEditor,
+  toolbarPlugin,
+  KitchenSinkToolbar,
+  listsPlugin,
+  quotePlugin,
+  headingsPlugin,
+  linkPlugin,
+  linkDialogPlugin,
+  imagePlugin,
+  tablePlugin,
+  thematicBreakPlugin,
+  frontmatterPlugin,
+  codeBlockPlugin,
+  sandpackPlugin,
+  codeMirrorPlugin,
+  directivesPlugin,
+  AdmonitionDirectiveDescriptor,
+  diffSourcePlugin,
+  markdownShortcutPlugin,
+  SandpackConfig,
+} from "@mdxeditor/editor";
+import "@mdxeditor/editor/style.css";
 
-function App() {
+export default function App() {
   return (
-    <MDXEditor markdown='Hello world' />
+    <MDXEditor
+      markdown={"# hello world"}
+      onChange={console.log}
+      plugins={[
+        toolbarPlugin({ toolbarContents: () => <KitchenSinkToolbar /> }),
+        listsPlugin(),
+        quotePlugin(),
+        headingsPlugin(),
+
+        linkPlugin(),
+        linkDialogPlugin(),
+        // eslint-disable-next-line @typescript-eslint/require-await
+        // imagePlugin({
+        //   imageUploadHandler: async () => '/sample-image.png',
+        // }),
+        tablePlugin(),
+        thematicBreakPlugin(),
+        frontmatterPlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "txt" }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            js: "JavaScript",
+            css: "CSS",
+            txt: "text",
+            tsx: "TypeScript",
+          },
+        }),
+        directivesPlugin({
+          directiveDescriptors: [AdmonitionDirectiveDescriptor],
+        }),
+        // diffSourcePlugin({ viewMode: 'rich-text', diffMarkdown }),
+        markdownShortcutPlugin(),
+      ]}
+    />
   );
 }
-
-export default App;
